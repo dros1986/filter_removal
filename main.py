2e09ccacb5fc825c56ba35ee31122327b0f2fbed
Flavio Piccoli <flavio.piccoli@disco.unimib.it>
2017-07-13 17:27:55 +0200

    Third degree. PatchSize=16.

--- main.py
+++ main.py
@@ -249,8 +249,8 @@ args = parser.parse_args()
 # ------------------ TRAIN ------------------
 # set parameters
 img_dim = [256,256]
-patchSize = 8
-nRow = 5 #10
+patchSize = 16
+nRow = 6 #10
 batchSize = nRow*nRow
 batchSizeVal = 50
 nepochs = 4
